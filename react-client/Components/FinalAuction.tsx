4f167c75908cb74614c18bd7f3ad530267089237
Barbara Gąsiewska <barbaragasiewska02@gmail.com>
2023-11-18 00:01:12 +0100

    Received form

---
+++ react-client/Components/FinalAuction.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import { outlinedInputClasses } from '@mui/material/OutlinedInput';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider, Theme, useTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const customTheme = (outerTheme: Theme) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            '--TextField-brandBorderColor': '#E0E3E7',
+            '--TextField-brandBorderHoverColor': '#B2BAC2',
+            '--TextField-brandBorderFocusedColor': '#6F7E8C',
+            '& label.Mui-focused': {
+              color: 'var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          notchedOutline: {
+            borderColor: 'var(--TextField-brandBorderColor)',
+          },
+          root: {
+            [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: 'var(--TextField-brandBorderHoverColor)',
+            },
+            [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: 'var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+      MuiFilledInput: {
+        styleOverrides: {
+          root: {
+            '&:before, &:after': {
+              borderBottom: '2px solid var(--TextField-brandBorderColor)',
+            },
+            '&:hover:not(.Mui-disabled, .Mui-error):before': {
+              borderBottom: '2px solid var(--TextField-brandBorderHoverColor)',
+            },
+            '&.Mui-focused:after': {
+              borderBottom: '2px solid var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+      MuiInput: {
+        styleOverrides: {
+          root: {
+            '&:before': {
+              borderBottom: '2px solid var(--TextField-brandBorderColor)',
+            },
+            '&:hover:not(.Mui-disabled, .Mui-error):before': {
+              borderBottom: '2px solid var(--TextField-brandBorderHoverColor)',
+            },
+            '&.Mui-focused:after': {
+              borderBottom: '2px solid var(--TextField-brandBorderFocusedColor)',
+            },
+          },
+        },
+      },
+    },
+  });
+
+function FinalAuction() {
+  const outerTheme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        display: 'block',
+        width: '90vw',
+        justifyItems: 'center',
+      }}
+    >
+      <ThemeProvider theme={customTheme(outerTheme)} >
+        <TextField fullWidth label="Price" sx={{
+            display: 'block',
+            margin: 5,
+          }}/>
+        <TextField multiline rows={4} fullWidth label="Description" sx={{
+            display: 'block',
+            margin: 5,
+          }}/>
+          <div style={{display: 'flex', width: '100vw', justifyContent: 'center'}}>
+            <Button variant="contained" sx={{marginRight: 10}}>Add auction</Button>
+            <Button variant="outlined">Decline auction</Button>
+        </div>
+          
+      </ThemeProvider>
+    </Box>
+  ); 
+}
+
+export default FinalAuction;
