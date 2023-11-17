5e29d08b7796135c335ff60c4f60f6569e241be8
Jan Stachurski <84142635+opprotossball@users.noreply.github.com>
2023-11-18 00:27:42 +0100

    Only auction info added (#10)

--- react-client/src/app/auction/details/[id]/page.tsx
+++ react-client/src/app/auction/details/[id]/page.tsx
@@ -2,9 +2,15 @@ import { Box, Button, Container, Grid, TextField } from "@mui/material";
 import ProductInfo from "@/components/product-info/productInfo";
 import BidInput from "@/components/bid-input/bidInput";
 import ProductQuestions from "@/components/product-questions/productQuestions";
+import AuctionInfo from "@/components/auction-info/auctionInfo";
 import PersonaLiveComponent from "@/components/persona-live-component/personaLiveComponent";
 
 export default function AuctionDetails({ params }: { params: { id: number } }) {
+  const bidders = [
+    { name: "Jhon Smith", price: 250 },
+    { name: "Lucas Tekk", price: 200 },
+    { name: "Monica Rose", price: 120 },
+  ];
   return (
     <Container component={"main"}>
       <Grid container spacing={2} pt={4} pb={4}>
@@ -16,6 +22,15 @@ export default function AuctionDetails({ params }: { params: { id: number } }) {
         </Grid>
         <Grid item xs={3}>
             <ProductInfo />
+        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid item xs={3}>
+          <AuctionInfo
+            currentPrice={999}
+            numberOfBidders={3}
+            topBidders={bidders}
+            endDate={new Date("2023-12-20T12:01:04.753Z")}
+          />
         </Grid>
       </Grid>
       <BidInput />
