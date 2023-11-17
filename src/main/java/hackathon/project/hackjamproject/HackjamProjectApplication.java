58948f8df8aaef32ccca056ea88acaaf11f81c3f
aidian3k <adrian.nowosielski1@gmail.com>
2023-11-17 15:47:51 +0100

    java project

---
+++ src/main/java/hackathon/project/hackjamproject/HackjamProjectApplication.java
@@ -0,0 +1,13 @@
+package hackathon.project.hackjamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackjamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackjamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hackathon/project/hackjamproject/HackjamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.project.hackjamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackjamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
