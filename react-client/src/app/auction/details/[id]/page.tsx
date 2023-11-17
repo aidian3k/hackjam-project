961ecebf7626d993e7a5d323e418223674dce894
dobos <doboszdav@gmail.com>
2023-11-17 20:25:59 +0100

    productInfo component added

--- react-client/src/app/auction/details/[id]/page.tsx
+++ react-client/src/app/auction/details/[id]/page.tsx
@@ -5,15 +5,11 @@ export default function AuctionDetails({ params }: { params: { id: number } }) {
   return (
     <Container component={"main"}>
       <Grid container spacing={2}>
-        <Grid item xs={2}>
+        <Grid item xs={3}>
           <ProductInfo />
         </Grid>
-        <Grid item xs={8}>
-          <ProductInfo />
-        </Grid>
-        <Grid item xs={2}>
-          <ProductInfo />
-        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid item xs={3}></Grid>
       </Grid>
     </Container>
   );
