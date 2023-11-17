43edf33e85a73cdb7ffc4f2624490540a43ebf0a
cezarys8 <czarek.skorupski2001@gmail.com>
2023-11-17 22:21:35 +0100

    AddAuctionComponent added

---
+++ react-client/src/app/auctions/add-auction/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import AddAuctionComponent from "../../../../components/AddAuctionComponent";
+
+
+const page = () => {
+
+    return (
+        <AddAuctionComponent />
+    )
+}
+
+export default page
