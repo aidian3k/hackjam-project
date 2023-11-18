022883dcba4daf1a79296c203ef79d41e9b642b8
Jan Stachurski <84142635+opprotossball@users.noreply.github.com>
2023-11-18 01:01:17 +0100

    Only auction info added (#12)

--- src/main/java/hackathon/project/hackjamproject/controller/AuctionController.java
+++ src/main/java/hackathon/project/hackjamproject/controller/AuctionController.java
@@ -1,10 +1,32 @@
 package hackathon.project.hackjamproject.controller;
 
+import hackathon.project.hackjamproject.dto.auction.AuctionCreationDTO;
+import hackathon.project.hackjamproject.entity.Auction;
+import hackathon.project.hackjamproject.entity.Media;
+import hackathon.project.hackjamproject.helpers.openai.dtos.ArtificialIntelligenceResponse;
+import hackathon.project.hackjamproject.service.AuctionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-class AuctionController {}
+class AuctionController {
+    private final AuctionService auctionService;
+
+    @GetMapping("/auction/{id}")
+    Auction getByID(@PathVariable Long id) {
+        return auctionService.findAuctionById(id);
+    }
+
+    @PostMapping("/auction/{userId}")
+    Auction create(@RequestBody AuctionCreationDTO auction, @PathVariable Long userId) {
+        return auctionService.handleCreateAuction(auction, userId);
+    }
+
+    @GetMapping("/auction/ai")
+    ArtificialIntelligenceResponse getAIResponse(@RequestBody Media media) {
+        return auctionService.getAuctionCoreInformationUsingPhoto(media);
+    }
+}
