f4ad41dc6e4beaa7c8a4c75c95e73d1957021ca6
Barbara Gąsiewska <barbaragasiewska02@gmail.com>
2023-11-18 05:56:10 +0100

    socket test

--- react-client/src/components/persona-live-component/personaLiveComponent.tsx
+++ react-client/src/components/persona-live-component/personaLiveComponent.tsx
@@ -1,7 +1,21 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 import Image from "next/image";
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
 export default function PersonaLiveComponent() {
+
+    const socket = new SockJS("")
+    const socketConnection = Stomp.over(socket);
+
+    socketConnection.connect({}, function (frame) {
+        console.log('Connected to SockJS and Stomp');
+        socketConnection.subscribe('/topic/example', function (message) {
+          console.log('Received message:', message.body);
+        });
+    })
+
+
     return (
         <Paper elevation={2}  sx={{ p: 2, height: '100%' }}>
             <Box style={{width: '100%', height: '100%', position: 'relative' }}>
