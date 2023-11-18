3b829bca064787cd13cd53df2c802e66765295b6
Jan Stachurski <jan.stachurski@outlook.com>
2023-11-18 15:22:50 +0100

    Added chat

--- react-client/src/components/product-questions/productQuestions.tsx
+++ react-client/src/components/product-questions/productQuestions.tsx
@@ -1,8 +1,73 @@
-import { Box, IconButton, Paper, Typography } from "@mui/material";
+"use client"
+import {Box, IconButton, LinearProgress, Paper, Typography} from "@mui/material";
 import QuestionInput from "@/components/product-questions/question-input/questionInput";
 import HelpIcon from "@mui/icons-material/Help";
+import {useEffect, useState} from "react";
+import {Media} from "@/app/auctions/add-auction/page";
+import axios from "axios";
 
-export default function ProductQuestions() {
+
+export type QuestionDTO = {
+  language: string,
+  auctionId: number,
+  question: string
+}
+
+export type QuestionResponseDTO = {
+  translatedResponse: string,
+  media: Media
+}
+
+
+export default function ProductQuestions(props: any) {
+  const [chatMessages, setChatMessages] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [currentText, setCurrentText] = useState("");
+  const [media, setMedia] = useState<Media | null>(null);
+
+
+    useEffect(() => {
+
+    }, [media?.imageUrl]);
+
+  function updateChatMessageAndPlay() {
+      setLoading(true);
+      const data: QuestionDTO = {
+        language: 'english',
+        auctionId: Number(props.auctionId),
+        question: currentText
+      }
+
+      axios.post<QuestionResponseDTO>("http://localhost:8080/api/question", data)
+          .then(result => result.data)
+          .then(result => {
+            setLoading(false);
+            setChatMessages(prevState => [...prevState, currentText, result.translatedResponse])
+            setCurrentText('');
+            props.setIsPlaying(true);
+            setMedia(result.media);
+          }).finally(() => {
+            setLoading(false);
+            setCurrentText('');
+      })
+  }
+
+  function displayMessages() {
+    const resultJsx = [];
+
+    for(let i =0 ; i < chatMessages.length; ++i) {
+      const message = chatMessages[i];
+
+      if(i % 2 != 0) {
+          resultJsx.push(<p>System : {message}</p>)
+      } else {
+        resultJsx.push(<p>Client : {message}</p>)
+      }
+    }
+
+    return resultJsx;
+  }
+
   return (
     <Paper elevation={2} sx={{ p: 3 }}>
       <Box>
@@ -13,13 +78,15 @@ export default function ProductQuestions() {
           <Typography variant={"h6"} fontWeight={"bold"}>
             Ask about product
           </Typography>
+            <audio className={'mx-2'} controls autoPlay={true} src={media?.imageUrl} onEnded={() => props.setIsPlaying(false)}/>
         </Box>
         <Typography ml={3} sx={{ color: "grey" }} mb={1}>
-          No questions asked
+          {chatMessages.length === 0 ? <p>No messages asked</p> : displayMessages()}
         </Typography>
       </Box>
 
-      <QuestionInput />
+      {loading ? <LinearProgress/> :
+      <QuestionInput onClick={updateChatMessageAndPlay} setCurrentValue={setCurrentText} value={currentText}/>}
     </Paper>
   );
 }
--- react-client/src/components/product-questions/question-input/questionInput.tsx
+++ react-client/src/components/product-questions/question-input/questionInput.tsx
@@ -1,11 +1,14 @@
+"use client"
 import { Box, IconButton, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
-export default function QuestionInput() {
+
+export default function QuestionInput(props: any) {
+
   return (
     <Box display={"flex"} alignItems={"center"} gap={1}>
-      <TextField fullWidth id="question" label="Question" variant="outlined" />
-      <IconButton>
+      <TextField fullWidth id="question" label="Question" variant="outlined" onChange={(e) => props.setCurrentValue(e.target.value)} value={props.value}/>
+      <IconButton onClick={props.onClick}>
         <SendIcon />
       </IconButton>
     </Box>
