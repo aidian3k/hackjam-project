b5a596b83d3292e338636d8f4b89b86bb8cca40e
Jan Stachurski <jan.stachurski@outlook.com>
2023-11-18 01:24:31 +0100

    User controller added

--- src/main/java/hackathon/project/hackjamproject/controller/UserController.java
+++ src/main/java/hackathon/project/hackjamproject/controller/UserController.java
@@ -1,10 +1,39 @@
 package hackathon.project.hackjamproject.controller;
 
+import hackathon.project.hackjamproject.dto.user.UserCreationDTO;
+import hackathon.project.hackjamproject.dto.user.UserDTO;
+import hackathon.project.hackjamproject.entity.User;
+import hackathon.project.hackjamproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-class UserController {}
+@RequestMapping("/api/")
+class UserController {
+    private final UserService userService;
+
+    @GetMapping("/user/{id}")
+    User getById(@PathVariable Long id) {
+       return userService.getUserById(id);
+    }
+
+    @GetMapping("/user/all")
+    List<UserDTO> getAll() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/user/update")
+    User update(User user) {
+        return userService.updateCurrentUser(user);
+    }
+
+    @PostMapping("/user")
+    UserCreationDTO create(UserCreationDTO userCreationDTO) {
+        return userService.createUser(userCreationDTO);
+    }
+}
